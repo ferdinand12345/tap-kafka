f59949fbd6f5204f30964622a3d1c798090c16ab
ferdinand12345 <ferdshinodas@gmail.com>
2019-09-24 13:51:10 +0700

    Perbaikan fungsi Consumer

--- consumer.php
+++ consumer.php
@@ -1,14 +1,20 @@
 <?php
-    require getcwd().'/vendor/autoload.php';
+	require getcwd().'/vendor/autoload.php';
 
-    $config = \Kafka\ConsumerConfig::getInstance();
-    $config->setMetadataRefreshIntervalMs(10000);
-    $config->setMetadataBrokerList('149.129.252.13:9092');
-    $config->setGroupId('ferdinand_topic_ebcc');
-    $config->setBrokerVersion('1.0.0');
-    $config->setTopics(['ferdinand_topic_ebcc']);
-    $consumer = new \Kafka\Consumer();
-
-    $consumer->start(function($topic, $part, $message) {
-        var_dump($message);
-    });
+	$Kafka = new RdKafka\Consumer();
+	$Kafka->setLogLevel(LOG_DEBUG);
+	$Kafka->addBrokers( "149.129.252.13" );
+	$Topic = $Kafka->newTopic( "testing" );
+	$Topic->consumeStart( 0, RD_KAFKA_OFFSET_BEGINNING );
+	while ( true ) {
+		$message = $Topic->consume( 0, 1000 );
+		if ( null === $message ) {
+			continue;
+		} elseif ( $message->err ) {
+			echo $message->errstr(), "\n";
+			break;
+		} else {
+			$payload = json_decode( $message->payload, true );
+			print_r($payload);
+		}
+	}
